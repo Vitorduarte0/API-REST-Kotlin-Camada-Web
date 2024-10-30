56b6218469706d4926ea348a9c80e9b5d5e9b1d3
Vitor Duarte Passo <vitor.passo@codex.com.br>
2024-10-29 23:00:12 -0300

    Trabalhando com JPA: Configurando JPA, editando as classes de dominino para o JPA e implementando repository

--- src/main/kotlin/br/com/alura/forum/services/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/services/TopicoService.kt
@@ -7,68 +7,49 @@ import br.com.alura.forum.exception.NotFoundException
 import br.com.alura.forum.mapper.TopicoFormMapper
 import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
+import br.com.alura.forum.repository.TopicoRepository
 import org.springframework.stereotype.Service
-
-// Para o spring conseguir gerenciar as coisas automatico é preciso que tenha pelo menos uma anotacao
 
 @Service
 class TopicoService(
-    private var topicos: MutableList<Topico>,
+    private var topicoRepository: TopicoRepository,
     private val topicoViewMapper: TopicoViewMapper,
     private val topicoFormMapper: TopicoFormMapper,
     private val notFoundMessage: String = "Topico Não encontrado!"
 ) {
 
     fun listarTopicos(): List<TopicoView> {
-        return topicos.map {topico: Topico ->
+        return topicoRepository.findAll().map { topico: Topico ->
             topicoViewMapper.map(topico)
         }
     }
 
     fun burcarTopicoPorId(id: Long): TopicoView {
-        return topicos.filter {
-            id == it.id
-        }.map { topico: Topico ->
+        return topicoRepository.findById(id).map { topico: Topico ->
             topicoViewMapper.map(topico)
-        }.first()
+        }.get()
     }
 
     fun cadastrar(topicoForm: TopicoForm): TopicoView {
         val topico = topicoFormMapper.map(topicoForm)
-        topico.id = topicos.size.toLong() + 1
-
-        topicos.add(topico)
+        topicoRepository.save(topico)
         return topicoViewMapper.map(topico)
     }
 
     fun atualizarTopico(atualizaForm: AtualizaForm): TopicoView {
-        val topico = topicos.first {topico ->
-            topico.id == atualizaForm.id
-        }
+        val topico = topicoRepository.findById(atualizaForm.id)
+            .orElseThrow { NotFoundException(notFoundMessage) }
 
-        val topicoAtualizado = Topico(
-            id = atualizaForm.id,
-            titulo = atualizaForm.titulo,
-            mensagem = atualizaForm.mensagem,
-            autor = topico.autor,
-            curso = topico.curso,
-            status = topico.status,
-            dataCriacao = topico.dataCriacao
-        )
-
-        topicos.remove(topico)
-        topicos.add(topicoAtualizado)
-
-        return topicoViewMapper.map(topicoAtualizado)
+        topico.titulo = atualizaForm.titulo
+        topico.mensagem = atualizaForm.mensagem
+        topicoRepository.save(topico)
+        return topicoViewMapper.map(topico)
     }
 
     fun deletarTopico(id: Long) {
-        //usando o stream do java para uasr o recurso do orElseThrow
-        val topico = topicos.stream()
-            .filter { topico ->  topico.id == id}
-            .findFirst()
+        val topico = topicoRepository.findById(id)
             .orElseThrow{NotFoundException(notFoundMessage)}
 
-        topicos.remove(topico)
+        topicoRepository.delete(topico)
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into CURSO (id, nome, categoria) values(1, 'Kotlin', 'PROGRAMACAO');
+insert into USUARIO (id, nome, email) values(1, 'Ana da Silva', 'ana@email.com');
