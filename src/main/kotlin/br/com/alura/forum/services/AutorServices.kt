550b2d03d88b5b6ffaa86ba086f8e96d696d1053
Vitor Duarte Passo <vitor.passo@codex.com.br>
2024-11-10 00:49:31 -0300

    Trabalhando com Spring Security: Realizando configurações de autenticaçao e adicionando novas migrações

--- src/main/kotlin/br/com/alura/forum/services/AutorServices.kt
+++ src/main/kotlin/br/com/alura/forum/services/AutorServices.kt
@@ -2,12 +2,20 @@ package br.com.alura.forum.services
 
 import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.repository.UsuarioRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
+import java.lang.RuntimeException
 
 @Service
-class AutorServices (private val autorRepository: UsuarioRepository) {
+class AutorServices (private val autorRepository: UsuarioRepository): UserDetailsService {
 
     fun buscarAutorPorId(idAutor: Long): Usuario {
         return autorRepository.findById(idAutor).get()
+    }
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val usuario = autorRepository.findByEmail(username) ?: throw RuntimeException()
+        return UserDetail(usuario)
     }
 }
---
+++ src/main/resources/db/migration/V6__insert_password_usuario.sql
@@ -0,0 +1,7 @@
+UPDATE
+    usuario
+ SET
+    password = '$2a$12$ZpqZILZDjtk8gMIBT8OndunJQGXrKO/Q82cCiMaqLIgNmSy3Pdh2C'
+WHERE
+    ID = 1
+;
